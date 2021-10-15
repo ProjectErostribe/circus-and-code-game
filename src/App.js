7aa04f1c82616a9ebabbe6f8cd1e90b271af8224
Paidamoyo Janet MYLES Azehko <78198093+ProjectErostribe@users.noreply.github.com>
2021-10-14 21:40:20 -0700

    App.js code added to development branch.  Has not been checked for functionality.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import logo from '.logo.svg';
+import '.App.css';
+import RoomData from '.components/RoomData.json';
+import EventsData from '.components/EventsData.json';
+import StartStory from '.components/StartStory';
 
 function App() {
+  const /* [What is something that the main character has to find in the story] */
+  const /* [What is something else that the main character has to find in the story]*/
+  const [scenes, setScenes] = useState(SceneData.scenes);
+  const [events, setEvents] = useState(EventsData.json);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <StartStory />
+        </Route>
+        <Route exact path="/">
+          <div>Scene</div>
+        </Route>
+        <Route path="/scene/:name">
+          <div>Space Name</div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
