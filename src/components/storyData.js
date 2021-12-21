35cad947ccd8d5e8fea7a0f37e5444e525998661
Paidamoyo Janet MYLES Azehko <78198093+ProjectErostribe@users.noreply.github.com>
2021-12-20 19:33:40 -0800

    Update storyData.js

--- src/components/storyData.js
+++ src/components/storyData.js
@@ -1,56 +1,59 @@
 export const storyData = [
     /* Startgame.js */
     [
-      `The Bitwise React Apprentices were out trick or treating on a dark and windy Halloween eve, when they stumbled across a creepy, rundown home at the end of a half-built construction site. Juan, the leader of the group, walked up to the door, rang the doorbell but no one answered.`,
+      `If I had an ayausca trip so deep, I'd want to wake up in the netherworld, Greeted by Alexander McQueen.`,
     ],
     [
-      `He jiggled the door and it swung open.`,
-      `"Is there anyone home?" asks Myles. "Do you think they have Skittles?"`,
-      `"Does it look like they have Skittles? They barely have time to clean from the looks of it!" Grace says, eyeing the cobwebs in the corner of the doorway.`,
-      `"Ooohh… I think I see some Skittles in the hallway!" Darla exclaims, pointing deeper into the darkened space just beyond the door.`,
+      `He'd tell me the secret to cheat death with art. He'd tell me all great things come from humble starts.`,
+    ],
+    [
+    `Remind me that the beginning's just the beginning and is always hard enough Because the end's only sweeter if it starts out rough.`,
+    ],
+    [
+        `The middle is where the glory is, All fearsome and wild. But fearsome is only fearsome when you're fame's golden child. The stars once above come glittering below. `,
+    ],
+    [
+        `The stars will turn out in fancy card to greet you on Saville Row.`,
+    ],
+    [
+        `And if this story is a story once put to the test, Investors and buyers wouldn't settle for less.`,
+    ],
+    [
+        `"Adieu my dear one, my mermaid song has been sung. I saw a mermaid that beaconed, And told me death had now come. For in my mind's eye my time had now come. But for you at this moment, your time's just begun."`,
     ]
   ]
-      /* Hallway.js */
-  export const hallway = [
+  /* Gameplay.js */
+  export const gameplay = [
     [
-      `The team of apprentices hesitantly step into the hallway, looking around into the begrimed space. The door slams shut behind them, shaking the dust from the ceiling above!`
+      /*` Story goes here`*/
     ],
     [
-      `A booming voice from the rafters cackles`,
-      `“You’ve found my Haunted House! Come in, grab a chair, take a nap if you'd like, but in order to leave you must find the silver key!”`
+      /*` Story goes here`*/
+    ],
+    
+  ]
+      /* Midway.js */
+  export const midway = [
+    [
+      /*` Story goes here`*/
     ],
     [
-      `“What was that?!” Habteab exclaimed.`,
-      `“Who said what about a key?” Garet says.`,
-      `“We still haven’t found out if they have Skittles…” Myles sighs, rolling their eyes.`
+      /*` Story goes here`*/
     ],
-    [
-      `The voice continues, “In front of you is a hallway filled with doors. Each room has a challenge for you all to beat. One room holds the key. Complete the challenge, find the key, and save yourselves before it’s too late. You’ve been volun-told to search the rooms for the key. Everyone else stays put.”`,
-    ],
-  [
-      `As you step forward, a large metal cage slams down around Juan and the rest of the group. A look of fear passes among the group.`,
-      `“Remember” the voice pronounces “Nobody belongs here more than you!”`
-    ]
+    
   ]
   /* Gamewon.js*/
   export const gamewon = [
-    `You return to the group with the key. The cage containing Juan and the rest of the team lifts. The team rushes out of the cage, and passes you to the door. As you hold out the key, the group parts and you spot the door. You insert the key into the keyhole and turn. As the door unlocks, you push the heavy door open. Fresh air rushes past you and into the house. You and the team spill out into the street as you spot a figure strolling up the street to the house.`,
-    `“Irma?! What are you doing here?!” Eyob questions.`,
-    `“Hi everyone. You’re early for the Bitwise Haunted House!” Irma says, a smile plastered on her face as she points to the small “Bitwise Industries” sign just above the doorway.`,
+    /*` Story goes here`*/
   ]
+
   /* FalseEnding.js */
   export const falseending = [
-    `You were tricked by the alien, play again to win the Silver Key!`,
+   /*` Story goes here`*/
   ]
-  /* HallwayReroute */
-  export const hallwayreroute = [
-    `In front of you is a hallway filled with doors. Each room has a challenge for you to beat.   One room holds the key. Complete the challenge, find the key, and make your way back to the hallway to present the key and save your friends and escape!`
+
+  /* MidwayReroute */
+  export const midwayreroute = [
+    /*` Story goes here`*/
   ]
-  /*
-  Things to add to this object:
-  StartGame
-  Hallway
-  GameWin
-  FalseEnding
-  Hallway Reroute page
-  */
+  
